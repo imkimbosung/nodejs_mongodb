b71339418f4bda923ced569ac3866f8ad775464f
김보성 <ghalsru@naver.com>
2019-03-01 05:14:40 +0900

    로그인, 로그아웃 추가

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 // res.send to res.render ( )
+
+// Welcome Page
 router.get('/', (req, res) => res.render('Welcome'));
+
+// Dashboard ( render 는 views 파일 안에 있는 파일이랑 연동)
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+  res.render('dashboard', {
+      name: req.user.name
+  }));
 
 module.exports = router;
