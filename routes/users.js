0599f738f712742379e673fad2588a30bdd68117
김보성 <ghalsru@naver.com>
2019-03-01 03:38:30 +0900

    비밀번호 암호화와 오타수정

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User module
+const User = require('../models/User');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
-
 
 // Register Page
 router.get('/register', (req, res) => res.render('register'));
@@ -40,7 +43,43 @@ router.post('/register', (req, res) =>{
         });
     }else {
        // Validation passed
-       
+       //  find email to db 
+        User.findOne({ email: email})
+         .then(user => {
+             if(user) {
+                // User exists
+                errors.push({ msg: 'Email is already registered'});
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                }); 
+             } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+
+                // Hash Password
+                bcrypt.genSalt(10, (err, salt) => 
+                   bcrypt.hash(newUser.password, salt, (err, hash) =>{
+                     if(err) throw err;
+                    // Set password to hashed
+                     newUser.password = hash;
+                    // Save user
+                     newUser.save()
+                        .then(user => {
+                            req.flash('success_msg', 'You are now registered and can log in');
+                            // 아이디 생성 후 로그인 페이지로 가는 곳
+                            res.redirect('/users/login');
+                        })
+                        .catch(err => console.log(err)); 
+                }))
+             }
+         });
     }
 
 });
